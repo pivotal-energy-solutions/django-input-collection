75a9fba9cc007b524a44051a6add657823c80980
Autumn <tonightslastsong@gmail.com>
2018-08-31 16:31:19 -0700

    Add package function to create result instances
    I expect this to be useful in user code, such as API endpoints that need to do this work without having to think too hard.

---
+++ input/collection.py
@@ -0,0 +1,21 @@
+from .models.utils import get_input_model
+
+
+CollectedInput = get_input_model()
+
+def store(instrument, data, **create_kwargs):
+    """
+    Creates a new CollectedInput instance for the given data.  Any additional kwargs are sent to
+    the manager's ``create()`` method, in case the CollectedInput model has been swapped and
+    requires additional fields to successfully save.
+    """
+    kwargs = {
+        'collection_request': instrument.collection_request,
+        'instrument': instrument,
+        'data': data,
+    }
+    kwargs.update(create_kwargs)
+
+    instance = CollectedInput.objects.create(**kwargs)
+
+    return instance
