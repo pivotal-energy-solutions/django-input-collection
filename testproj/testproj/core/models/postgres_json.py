99c04f0618d46b82e3b3b5747b012cea10e304cc
Autumn <tonightslastsong@gmail.com>
2018-08-31 14:45:02 -0700

    Inherit from the correct base

--- testproj/testproj/core/models/postgres_json.py
+++ testproj/testproj/core/models/postgres_json.py
@@ -1,9 +1,9 @@
 from django.contrib.postgres.fields import JSONField
 
-from input.models import CollectedInput
+from input.models import AbstractCollectedInput
 
 __all__ = ['CollectedInput_Postgres_JSON']
 
 
-class CollectedInput_Postgres_JSON(CollectedInput):
+class CollectedInput_Postgres_JSON(AbstractCollectedInput):
     data = JSONField()
