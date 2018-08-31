6b125bc77340b04fd3475219826869a6a246d006
Autumn <tonightslastsong@gmail.com>
2018-08-31 14:42:54 -0700

    Fix typo

--- testproj/testproj/core/models/mysql_json.py
+++ testproj/testproj/core/models/mysql_json.py
@@ -3,7 +3,7 @@ from django.db import models
 from django_mysql.models import JSONField, QuerySet as JsonAware_QuerySet
 from input.models import AbstractCollectedInput
 
-__all__ = ['CollectedInput_Postgres_JSON']
+__all__ = ['CollectedInput_MySQL_JSON']
 
 
 class CollectedInput_MySQL_JSON(AbstractCollectedInput):
