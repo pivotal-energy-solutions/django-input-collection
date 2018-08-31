47726c59d9b767439d39f7f50112cc9478feaa5b
Autumn <tonightslastsong@gmail.com>
2018-08-31 14:38:40 -0700

    Specify * imports

--- testproj/testproj/core/models/mysql_json.py
+++ testproj/testproj/core/models/mysql_json.py
@@ -3,6 +3,8 @@ from django.db import models
 from django_mysql.models import QuerySet as JsonAware_QuerySet
 from input.models import CollectedInput
 
+__all__ = ['CollectedInput_Postgres_JSON']
+
 
 class CollectedInput_MySQL_JSON(CollectedInput):
     objects = JsonAware_QuerySet.as_manager()
--- testproj/testproj/core/models/postgres_json.py
+++ testproj/testproj/core/models/postgres_json.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from input.models import CollectedInput
 
+__all__ = ['CollectedInput_Postgres_JSON']
+
 
 class CollectedInput_Postgres_JSON(CollectedInput):
     data = models.JSONField()
