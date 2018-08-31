f4d3a57b6c68b7b2686ad8dd14a902f2b2f79c87
Autumn <tonightslastsong@gmail.com>
2018-08-31 15:02:07 -0700

    Move urls to package

---
+++ testproj/testproj/urls/base.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import api
+
+
+urlpatterns = [
+    path('', include('testproj.core.urls')),
+
+    # Temporary
+    path('admin/', admin.site.urls),
+]
