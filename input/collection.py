7882fbfc6b24188ae0f9b7dec193e58114a8cbd6
Autumn <tonightslastsong@gmail.com>
2018-08-31 17:07:10 -0700

    Move model lookup out of global execution

--- input/collection.py
+++ input/collection.py
@@ -1,14 +1,14 @@
 from .models.utils import get_input_model
 
 
-CollectedInput = get_input_model()
-
 def store(instrument, data, **create_kwargs):
     """
     Creates a new CollectedInput instance for the given data.  Any additional kwargs are sent to
     the manager's ``create()`` method, in case the CollectedInput model has been swapped and
     requires additional fields to successfully save.
     """
+    CollectedInput = get_input_model()
+
     kwargs = {
         'collection_request': instrument.collection_request,
         'instrument': instrument,
