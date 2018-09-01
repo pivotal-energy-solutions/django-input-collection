19b4dcc643e4198c18ed1f25fb3d1d2711aeda56
Autumn <tonightslastsong@gmail.com>
2018-08-31 17:45:33 -0700

    Show all fields on default serializers

--- input/api/restframework/serializers.py
+++ input/api/restframework/serializers.py
@@ -6,23 +6,28 @@ from ... import models
 class MeasureSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Measure
+        fields = '__all__'
 
 
 class CollectionGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CollectionGroup
+        fields = '__all__'
 
 
 class CollectionRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CollectionRequest
+        fields = '__all__'
 
 
 class CollectionInstrumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CollectionInstrument
+        fields = '__all__'
 
 
 class CollectionInputSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.get_input_model()
+        fields = '__all__'
