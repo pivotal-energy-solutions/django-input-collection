1d2337c5ba8ad828db7fb551256faaa5ff290920
Autumn <tonightslastsong@gmail.com>
2018-08-31 17:46:09 -0700

    Register basic api using default viewsets

--- testproj/testproj/urls/api.py
+++ testproj/testproj/urls/api.py
@@ -1,3 +1,11 @@
 from rest_framework import routers
 
+from input.api.restframework import api
+
+
 router = routers.SimpleRouter()
+router.register(r'measure', api.MeasureViewSet, base_name='measure')
+router.register(r'group', api.CollectionGroupViewSet, base_name='group')
+router.register(r'collection-request', api.CollectionRequestViewSet, base_name='collection-request')
+router.register(r'instrument', api.CollectionInstrumentViewSet, base_name='instrument')
+router.register(r'input', api.CollectionInputViewSet, base_name='input')
