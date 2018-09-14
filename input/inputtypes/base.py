31f3c37b545887c3d92bf7f26a4b4802579eb82c
Autumn <tonightslastsong@gmail.com>
2018-09-14 16:40:32 -0700

    Add Collector class for housing collection details

--- input/inputtypes/base.py
+++ input/inputtypes/base.py
@@ -1,16 +1,36 @@
+from django.forms.models import model_to_dict
 
-__all__ = ['InputMethod', 'register']
-
-
-#############
-# TEMPORARY #
-#############
+__all__ = ['Collector', 'InputMethod']
 
 
-_input_methods = set()
+# _input_methods = set()
+#
+# def register(inputmethod_class):
+#     _input_methods.add(inputmethod_class)
 
-def register(inputmethod_class):
-    _input_methods.add(inputmethod_class)
+
+class Collector(object):
+    __version__ = (0, 0, 0, 'dev')
+    group = 'default'
+
+    def __init__(self, collection_request, user=None, **kwargs):
+        self.collection_request = collection_request
+        self.user = user
+
+        if 'group' in kwargs:
+            self.group = kwargs['group']
+
+    @property
+    def info(self):
+        """ Returns a JSON-safe spec for another tool to correctly supply inputs. """
+        info = {
+            'meta': {
+                'version': self.__version__,
+            },
+            'collection_request': model_to_dict(self.collection_request),
+            'group': self.group,
+        }
+        return info
 
 
 class InputMethod(object):
