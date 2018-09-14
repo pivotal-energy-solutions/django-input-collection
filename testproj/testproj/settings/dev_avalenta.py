e1a09c320e1dc42b37f5ab1d1a43531b841e5d7b
Autumn <tonightslastsong@gmail.com>
2018-09-14 16:42:38 -0700

    Switch to mysql 5.7 json model

--- testproj/testproj/settings/dev_avalenta.py
+++ testproj/testproj/settings/dev_avalenta.py
@@ -1,19 +1,19 @@
 from .dev import *
 
 # Default mode
-INPUT_COLLECTEDINPUT_MODEL = 'input.CollectedInput'
+# INPUT_COLLECTEDINPUT_MODEL = 'input.CollectedInput'
 
 # MySQL mode
-# DATABASES['default'] = {
-#     'ENGINE': 'django.db.backends.mysql',
-#     'NAME': 'input_testproj',
-#     'USERNAME': env.get_variable('DATABASE_USERNAME', 'root'),
-#     'PASSWORD': env.get_variable('DATABASE_PASSWORD', ''),
-# }
-# INSTALLED_APPS.extend([
-#     'django_mysql',
-# ])
-# INPUT_COLLECTEDINPUT_MODEL = 'testproj.CollectedInput_MySQL_JSON'
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'input_testproj',
+    'USER': env.get_variable('DATABASE_USERNAME', 'root'),
+    'PASSWORD': env.get_variable('DATABASE_PASSWORD', ''),
+}
+INSTALLED_APPS.extend([
+    'django_mysql',
+])
+INPUT_COLLECTEDINPUT_MODEL = 'core.CollectedInput_MySQL_JSON'
 
 
 # Postgres mode
