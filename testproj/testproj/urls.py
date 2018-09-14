f08a5b24bc9a962be2992a526e2146a03544bbb7
Autumn <tonightslastsong@gmail.com>
2018-09-14 16:42:19 -0700

    Flatten urls module

--- testproj/testproj/urls.py
+++ testproj/testproj/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from . import api
-
 
 urlpatterns = [
     path('', include('testproj.core.urls')),
 
     # API
-    path('api/', include((api.router.urls, 'api'), namespace='api')),
+    path('api/', include('input.api.restframework.urls')),
 
     # Temporary
     path('admin/', admin.site.urls),
