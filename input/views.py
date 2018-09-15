df434c85332fd4859c4067fb763edc90de98fdad
Autumn <tonightslastsong@gmail.com>
2018-09-14 17:59:06 -0700

    Add base CollectorView

---
+++ input/views.py
@@ -0,0 +1,28 @@
+from django.views.generic import DetailView
+
+from . import models
+
+
+class CollectorView(DetailView):
+    model = models.CollectionRequest
+    collector_class = None
+
+    def get_collector_class(self):
+        return self.collector_class
+
+    def get_collector_kwargs(self, **kwargs):
+        kwargs.update({
+            'collection_request': self.object,
+        })
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super(CollectorView, self).get_context_data(**kwargs)
+
+        collector_class = self.get_collector_class()
+        collector_kwargs = self.get_collector_kwargs()
+        collector = collector_class(**collector_kwargs)
+
+        context['payload'] = collector.info
+
+        return context
