d5e6a1a1eec4cd15dcb3c83deb959efe33730006
Autumn <tonightslastsong@gmail.com>
2018-09-14 17:58:46 -0700

    Add deeper info gathering for response info

--- input/inputtypes/base.py
+++ input/inputtypes/base.py
@@ -23,14 +23,45 @@ class Collector(object):
     @property
     def info(self):
         """ Returns a JSON-safe spec for another tool to correctly supply inputs. """
+        collection_request_info = model_to_dict(self.collection_request)
+        instruments_info = self.get_instruments()
+
         info = {
             'meta': {
                 'version': self.__version__,
             },
-            'collection_request': model_to_dict(self.collection_request),
+            'collection_request': collection_request_info,
             'group': self.group,
+            'instruments': instruments_info,
         }
         return info
+
+    def get_instruments(self):
+        instruments_info = []
+
+        queryset = self.collection_request.collectioninstrument_set.all()  # FIXME: valuesqueryset
+        for instrument in queryset:
+            info = model_to_dict(instrument)
+            info['response_info'] = self.get_instrument_input_info(instrument)
+            instruments_info.append(info)
+
+        return instruments_info
+
+    def get_instrument_input_info(self, instrument):
+        """ Returns input specifications for data this instruments wants to collect. """
+        policy_info = model_to_dict(instrument.response_policy)
+        suggested_responses_info = self.get_suggested_responses_info(instrument)
+
+        input_info = {
+            'response_policy': policy_info,
+            'suggested_responses': suggested_responses_info,
+        }
+        return input_info
+
+    def get_suggested_responses_info(self, instrument):
+        queryset = instrument.suggested_responses.all()
+        suggested_responses_info = list(map(model_to_dict, queryset))
+        return suggested_responses_info
 
 
 class InputMethod(object):
