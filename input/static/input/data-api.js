5e9db2281e1b04dc60aa3deef2a77af8ed900e97
Autumn <tonightslastsong@gmail.com>
2018-09-17 12:56:29 -0700

    Add base api js
    This is is meant to be useable directly if desired, but also to easily slot into some bigger framework that will handle the xhr part itself.

---
+++ input/static/input/data-api.js
@@ -0,0 +1,52 @@
+var DjangoInputCollection = (function(){
+    var api = {
+        endpointPrefix: '/api',
+        endpointPaths: {
+            submit: '/input/'
+        },
+        endpointMethods: {
+            submit: 'post'
+        },
+        payloadGetters: {
+            submit: function(instrumentId, data) {
+                return {
+                    instrument: instrumentId,
+                    data: data
+                };
+            }
+        },
+        api: {
+            // One-liners for sending archetypical requests
+            submit: function(instrumentId, data) {
+                return api.sendRequest('submit', instrumentId, data);
+            }
+        },
+
+        getRequestArgs: function(type, data) {
+            return {
+                url: api.endpointPrefix + api.endpointPaths[type],
+                method: api.endpointMethods[type],
+                data: data
+            };
+        },
+        getRequest: function(type, method, url) {
+            var xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+            return xhr;
+        },
+        sendRequest: function(type, ...dataArgs) {
+            var data = api.payloadGetters[type](...dataArgs);
+            var requestArgs = api.getRequestArgs(type, data);
+            var xhr = api.getRequest(type, requestArgs.method, requestArgs.url);
+            var postString = undefined;
+            if (requestArgs.method == 'post' || requestArgs.method == 'put') {
+                xhr.setRequestHeader('Content-Type', 'application/json');
+                postString = JSON.stringify(data);
+            }
+            xhr.send(postString);
+            return xhr;
+        },
+    };
+
+    return api;
+})();
