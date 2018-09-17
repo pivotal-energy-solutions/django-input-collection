ef84c37a3cc0f4324a8f062838dd5cf915e0e627
Autumn <tonightslastsong@gmail.com>
2018-09-17 11:34:27 -0700

    Add another specific case, reorder, drop extras

--- testproj/create_objects.py
+++ testproj/create_objects.py
@@ -11,23 +11,32 @@ instrument_kwargs = {
     'response_policy__nickname': 'default',
 }
 
+# Default, no suggestions
 factories.CollectionInstrumentFactory.create(**instrument_kwargs)
+
+# Open response with suggestions
+factories.CollectionInstrumentFactory.create(**dict(instrument_kwargs, **{
+    'suggested_responses': suggested_responses,
+}))
+
+# Multiple choice, no "Other" answer
 factories.CollectionInstrumentFactory.create(**dict(instrument_kwargs, **{
     'response_policy__nickname': 'restrict',
     'response_policy__restrict': True,
     'suggested_responses': suggested_responses,
 }))
+
+# Multiple choice, select-multiple
 factories.CollectionInstrumentFactory.create(**dict(instrument_kwargs, **{
     'response_policy__nickname': 'multiple-restrict',
     'response_policy__multiple': True,
     'response_policy__restrict': True,
     'suggested_responses': suggested_responses,
 }))
+
+# Multiple choice, select-multiple, "Other" allowed
 factories.CollectionInstrumentFactory.create(**dict(instrument_kwargs, **{
     'response_policy__nickname': 'multiple',
     'response_policy__multiple': True,
-    'suggested_responses': suggested_responses,
-}))
-factories.CollectionInstrumentFactory.create_batch(size=6, **dict(instrument_kwargs, **{
     'suggested_responses': suggested_responses,
 }))
