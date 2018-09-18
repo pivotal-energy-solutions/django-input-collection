6f64da94c075ec3c2b33320f9e85e3799bf29559
Autumn <tonightslastsong@gmail.com>
2018-09-18 12:41:24 -0700

    Move base Collector classes to separate module

--- input/collection.py
+++ input/collection.py
@@ -1,4 +1,8 @@
+from django.forms.models import model_to_dict
+
 from .models.utils import get_input_model
+
+__all__ = ['Collector', 'store']
 
 
 def store(instrument, data, instance=None, **model_kwargs):
@@ -26,3 +30,72 @@ def store(instrument, data, instance=None, **model_kwargs):
     instance, created = CollectedInput.objects.update_or_create(pk=pk, defaults=kwargs)
 
     return instance
+
+
+class Collector(object):
+    __version__ = (0, 0, 0, 'dev')
+    group = 'default'
+
+    def __init__(self, collection_request, user=None, **kwargs):
+        self.collection_request = collection_request
+        self.user = user
+
+        if 'group' in kwargs:
+            self.group = kwargs['group']
+
+    @property
+    def info(self):
+        """ Returns a JSON-safe spec for another tool to correctly supply inputs. """
+        meta_info = self.get_meta()
+        collection_request_info = model_to_dict(self.collection_request)
+        instruments_info = self.get_instruments()
+
+        info = {
+            'meta': meta_info,
+            'collection_request': collection_request_info,
+            'group': self.group,
+            'instruments': instruments_info,
+        }
+        return info
+
+    def get_meta(self):
+        return {
+            'version': self.__version__,
+        }
+
+    def get_instruments(self):
+        instruments_info = []
+
+        queryset = self.collection_request.collectioninstrument_set.all()  # FIXME: valuesqueryset
+        for instrument in queryset:
+            info = model_to_dict(instrument)
+            info['response_info'] = self.get_instrument_input_info(instrument)
+            instruments_info.append(info)
+
+        return instruments_info
+
+    def get_instrument_input_info(self, instrument):
+        """ Returns input specifications for data this instruments wants to collect. """
+        policy_info = model_to_dict(instrument.response_policy)
+        suggested_responses_info = self.get_suggested_responses_info(instrument)
+
+        input_info = {
+            'response_policy': policy_info,
+            'suggested_responses': suggested_responses_info,
+        }
+        return input_info
+
+    def get_suggested_responses_info(self, instrument):
+        queryset = instrument.suggested_responses.all()
+        suggested_responses_info = list(map(model_to_dict, queryset))
+        return suggested_responses_info
+
+
+class BaseAPICollector(Collector):
+    def get_meta(self):
+        meta_info = super(BaseAPICollector, self).get_meta()
+        meta_info['api'] = self.get_api_info()
+        return meta_info
+
+    def get_api_info(self):
+        return {}
--- input/inputtypes/base.py
+++ input/inputtypes/base.py
@@ -1,67 +1,10 @@
-from django.forms.models import model_to_dict
-
-__all__ = ['Collector', 'InputMethod']
+__all__ = ['InputMethod']
 
 
 # _input_methods = set()
 #
 # def register(inputmethod_class):
 #     _input_methods.add(inputmethod_class)
-
-
-class Collector(object):
-    __version__ = (0, 0, 0, 'dev')
-    group = 'default'
-
-    def __init__(self, collection_request, user=None, **kwargs):
-        self.collection_request = collection_request
-        self.user = user
-
-        if 'group' in kwargs:
-            self.group = kwargs['group']
-
-    @property
-    def info(self):
-        """ Returns a JSON-safe spec for another tool to correctly supply inputs. """
-        collection_request_info = model_to_dict(self.collection_request)
-        instruments_info = self.get_instruments()
-
-        info = {
-            'meta': {
-                'version': self.__version__,
-            },
-            'collection_request': collection_request_info,
-            'group': self.group,
-            'instruments': instruments_info,
-        }
-        return info
-
-    def get_instruments(self):
-        instruments_info = []
-
-        queryset = self.collection_request.collectioninstrument_set.all()  # FIXME: valuesqueryset
-        for instrument in queryset:
-            info = model_to_dict(instrument)
-            info['response_info'] = self.get_instrument_input_info(instrument)
-            instruments_info.append(info)
-
-        return instruments_info
-
-    def get_instrument_input_info(self, instrument):
-        """ Returns input specifications for data this instruments wants to collect. """
-        policy_info = model_to_dict(instrument.response_policy)
-        suggested_responses_info = self.get_suggested_responses_info(instrument)
-
-        input_info = {
-            'response_policy': policy_info,
-            'suggested_responses': suggested_responses_info,
-        }
-        return input_info
-
-    def get_suggested_responses_info(self, instrument):
-        queryset = instrument.suggested_responses.all()
-        suggested_responses_info = list(map(model_to_dict, queryset))
-        return suggested_responses_info
 
 
 class InputMethod(object):
