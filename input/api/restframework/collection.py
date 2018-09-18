6aa1de0e2af2357beb105dc600016ea881e9e515
Autumn <tonightslastsong@gmail.com>
2018-09-18 12:42:43 -0700

    Stub DRF-specific API Collector

---
+++ input/api/restframework/collection.py
@@ -0,0 +1,22 @@
+from django.urls import reverse
+
+from ...collection import BaseAPICollector
+
+
+class RestFrameworkCollector(BaseAPICollector):
+    def get_api_info(self):
+        placeholder_pk_kwargs = {'pk': '__id__'}
+
+        input_list = reverse('api:input-list')
+        input_detail = reverse('api:input-detail', kwargs=placeholder_pk_kwargs)
+
+        return {
+            'content_type': 'json',
+            'endpoints': {
+                'input': {
+                    'list': {'url': input_list, 'method': 'GET'},
+                    'add': {'url': input_list, 'method': 'POST'},
+                    'get': {'url': input_detail, 'method': 'GET'},
+                },
+            },
+        }
