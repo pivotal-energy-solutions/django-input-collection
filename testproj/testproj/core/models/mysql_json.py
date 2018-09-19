790db2e2f6d4ca952408bc6f8485a68e9a29c5a9
Autumn <tonightslastsong@gmail.com>
2018-09-19 16:52:39 -0700

    Use the built-in Latest manager for testing purposes

--- testproj/testproj/core/models/mysql_json.py
+++ testproj/testproj/core/models/mysql_json.py
@@ -1,10 +1,12 @@
 from django_mysql.models import JSONField, QuerySet as JsonAware_QuerySet
 from input.models import AbstractCollectedInput
+from input.models.managers import UserLatestCollectedInputQuerySet
 
 __all__ = ['CollectedInput_MySQL_JSON']
 
 
 class CollectedInput_MySQL_JSON(AbstractCollectedInput):
     objects = JsonAware_QuerySet.as_manager()
+    filtered_objects = UserLatestCollectedInputQuerySet.as_manager()
 
     data = JSONField()
