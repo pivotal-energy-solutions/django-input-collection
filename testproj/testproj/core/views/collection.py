031e4aca1488293b1d5e7c4d70b03706483004c4
Autumn <tonightslastsong@gmail.com>
2018-09-20 15:40:39 -0700

    Pre-process instruments info so template has an easier time

--- testproj/testproj/core/views/collection.py
+++ testproj/testproj/core/views/collection.py
@@ -1,5 +1,14 @@
 from input.api.restframework import collection
 
 
-    pass
 class PollTemplateViewCollector(collection.RestFrameworkCollector):
+    @property
+    def info(self):
+        info = super(PollTemplateViewCollector, self).info
+
+        # The default Django template language stuff makes it impossible to look up an index, so
+        # this saves us the heartache of trying.
+        info['instruments_info']['ordered_instruments'] = [
+            info['instruments_info']['instruments'][id] for id in info['instruments_info']['ordering']
+        ]
+        return info
