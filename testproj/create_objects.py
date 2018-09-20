2714daf6876270d7d6a1c0f2a12ff2f0a0cbb73e
Autumn <tonightslastsong@gmail.com>
2018-09-20 14:35:38 -0700

    Add conditionals

--- testproj/create_objects.py
+++ testproj/create_objects.py
@@ -18,17 +18,79 @@ suggested_responses = [
     factories.SuggestedResponseFactory.create(data='Maybe'),
 ]
 
+
+# Triggers when suggested responses only
+casegroup_only_suggested = factories.CaseGroupFactory.create(**{
+    'id': 'gimme-only-suggested',
+    'require_all': True,
+    'condition_group': factories.ConditionGroupFactory.create(**{
+        'id': 'gimme-only-suggested',
+        'require_all': True,
+    }),
+    'cases': [factories.CaseFactory.create(has_response_type='all-suggested')],
+})
+# Triggers when at least one suggested response
+casegroup_any_suggested = factories.CaseGroupFactory.create(**{
+    'id': 'gimme-one-suggested',
+    'require_all': True,
+    'condition_group': factories.ConditionGroupFactory.create(**{
+        'id': 'gimme-one-suggested',
+        'require_all': True,
+    }),
+    'cases': [factories.CaseFactory.create(has_response_type='one-suggested')],
+})
+# Triggers when custom responses only
+casegroup_only_custom = factories.CaseGroupFactory.create(**{
+    'id': 'gimme-only-custom',
+    'require_all': True,
+    'condition_group': factories.ConditionGroupFactory.create(**{
+        'id': 'gimme-only-custom',
+        'require_all': True,
+    }),
+    'cases': [factories.CaseFactory.create(has_response_type='all-custom')],
+})
+# Triggers when at least one custom response
+casegroup_any_custom = factories.CaseGroupFactory.create(**{
+    'id': 'gimme-one-custom',
+    'require_all': True,
+    'condition_group': factories.ConditionGroupFactory.create(**{
+        'id': 'gimme-one-custom',
+        'require_all': True,
+    }),
+    'cases': [factories.CaseFactory.create(has_response_type='one-custom')],
+})
+
+
+
+
 instrument_kwargs = {
     'collection_request__id': 1,
     'response_policy__nickname': 'default',
 }
 
 # Default, no suggestions
-factories.CollectionInstrumentFactory.create(**instrument_kwargs)
+factories.CollectionInstrumentFactory.create(**dict(instrument_kwargs, **{
+}))
 
 # Open response with suggestions
 factories.CollectionInstrumentFactory.create(**dict(instrument_kwargs, **{
     'suggested_responses': suggested_responses,
+}))
+factories.CollectionInstrumentFactory.create(**dict(instrument_kwargs, **{
+    'suggested_responses': suggested_responses,
+    'condition_set': [factories.ConditionFactory.create(**{
+        'condition_group__id': 'gimme-only-suggested',
+        'instrument': factories.CollectionInstrumentFactory.create(**dict(instrument_kwargs, **{
+        })),
+    })],
+}))
+factories.CollectionInstrumentFactory.create(**dict(instrument_kwargs, **{
+    'suggested_responses': suggested_responses,
+    'condition_set': [factories.ConditionFactory.create(**{
+        'condition_group__id': 'gimme-only-custom',
+        'instrument': factories.CollectionInstrumentFactory.create(**dict(instrument_kwargs, **{
+        })),
+    })],
 }))
 
 # Multiple choice, no "Other" answer
