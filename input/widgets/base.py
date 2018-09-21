fe7ffcc10dc027d440a4a16ff92950891eee9a1e
Autumn <tonightslastsong@gmail.com>
2018-09-21 15:12:18 -0700

    Switch to UserDict base class
    This represents what we need anyway for easy serialization, but we still want an attribute API so it's not jarring to work with.

--- input/widgets/base.py
+++ input/widgets/base.py
@@ -1,10 +1,13 @@
-__all__ = ['InputMethod']
+from collections import UserDict
 
 __all__ = ['InputMethod', 'Widget']
 
 
+class missing(object):
+    pass
 
-class InputMethod(object):
+
+class InputMethod(UserDict):
     """
     A stateless encapsulation of the components required to obtain and coerce data for an arbitrary
     CollectionInstrument.  Its job is to produce something that supports interaction through
@@ -13,11 +16,27 @@ class InputMethod(object):
     """
 
     def __init__(self, **kwargs):
+        super(InputMethod, self).__init__(**kwargs)
         self.update_kwargs(**kwargs)
+
+    def __getattr__(self, k):
+        if k == 'data':
+            return self.data
+
+        return self.data[k]
+
+    def __setattr__(self, k, v):
+        super(InputMethod, self).__setattr__(k, v)
+
+        if k == 'data':
+            return
+
+        self.data[k] = v
 
     def update_kwargs(self, **kwargs):
         for k, v in kwargs.items():
-            if not hasattr(self, k):
+            attr = getattr(self, k, missing)
+            if attr is missing or k.startswith('_') or callable(attr):
                 raise AttributeError("Invalid attribute %r for widget %r" % (k, self))
             setattr(self, k, v)
 
