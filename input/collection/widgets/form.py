aed2af0069e752461edcf231a8028e34c5d8d425
Autumn <tonightslastsong@gmail.com>
2018-09-21 16:32:38 -0700

    Streamline

--- input/collection/widgets/form.py
+++ input/collection/widgets/form.py
@@ -26,16 +26,25 @@ class FormFieldWidget(InputMethod):
     def get_formfield(self):
         return self.formfield_class()
 
-    def serialize(self):
-        data = super(FormFieldWidget, self).serialize()
+    def serialize(self, instrument):
+        # NOTE: If we can ensure an easy design where the <input> names don't need to be scoped by
+        # the [name="instrument-{pk}"], we could potentially drop this ``instrument`` arg.
+
+        data = super(FormFieldWidget, self).serialize(instrument)
+
+        # Remove hard reference to class and create a reasonable serialization here
+        del data['formfield_class']
 
         field = self.get_formfield()
-        info = {
-            'default_error_messages': field.default_error_messages,
-            'empty_values': field.empty_values,
-            'help_text': field.empty_values,
-            'widget_attrs': field.widget_attrs({}),
-        }
+        known_attrs = ['max_length', 'min_length', 'empty_values', 'help_text', 'input_formats']
+        for attr in known_attrs:
+            if not hasattr(field, attr):
+                continue
+            data[attr] = getattr(field, attr)
 
-        data['formfield_class'] = info
+        data['meta'].update({
+            'widget_class': field.widget.__class__.__name__,
+            'field_class': field.__class__.__name__,
+        })
+
         return data
