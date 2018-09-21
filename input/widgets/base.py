ff2fc936628383bcdf19ecc843a0acd612425978
Autumn <tonightslastsong@gmail.com>
2018-09-21 12:44:14 -0700

    Remove old code

--- input/widgets/base.py
+++ input/widgets/base.py
@@ -1,10 +1,6 @@
 __all__ = ['InputMethod']
 
 
-# _input_methods = set()
-#
-# def register(inputmethod_class):
-#     _input_methods.add(inputmethod_class)
 
 
 class InputMethod(object):
@@ -24,22 +20,6 @@ class InputMethod(object):
                 raise AttributeError("Invalid attribute %r for widget %r" % (k, self))
             setattr(self, k, v)
 
-    def petition(self, instrument):
-        raise NotImplemented
-
-    def save(self, instrument, result):
-        cleaned_result = self.validate(result)  # TODO: ValidationError workflow?
-        # TODO: trigger creation of CollectedInput.  This must happen externally so that custom
-        # model swaps can handle this as required.
-
     def validate(self, result):
         """ Validate the result and perform any necessary type coercion. """
         return result
-
-
-# def interact(inputmethod, instrument):
-#     if callable(inputmethod):
-#         inputmethod = inputmethod()
-#
-#     result = inputmethod.petition(instrument)
-#     inputmethod.recieve(instrument, result)
