f54915e27c58422ffa877c3a4ca198d3714d6a71
Autumn <tonightslastsong@gmail.com>
2018-09-21 15:10:12 -0700

    Add a json encoder (again)
    Thought we might sneak by without this, but proxy/lazy translation strings are unreasonable to catch elsewhere.

---
+++ input/json.py
@@ -0,0 +1,14 @@
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Model
+from django.utils.functional import Promise
+from django.utils.encoding import force_text
+
+
+class CollectionSpecificationJSONEncoder(DjangoJSONEncoder):
+    def default(self, o):
+        if isinstance(o, Model):
+            return model_to_dict(o)
+        elif isinstance(o, Promise):  # Catch reverse_lazy, among other simple things
+            return force_text(o)
+
+        return super(CollectionSpecificationJSONEncoder, self).default(o)
