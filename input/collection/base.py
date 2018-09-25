4c6aed0dc442fa2ba20a4dc5ea92f949782d6716
Autumn <tonightslastsong@gmail.com>
2018-09-25 10:43:08 -0700

    Move model resolution to global space

--- input/collection/base.py
+++ input/collection/base.py
@@ -3,13 +3,14 @@ from .. import models
 __all__ = ['store', 'get_data_for_suggested_responses', 'test_condition_case']
 
 
+CollectedInput = models.get_input_model()
+
 def store(instrument, data, instance=None, **model_kwargs):
     """
     Creates a new CollectedInput instance for the given data.  Any additional kwargs are sent to
     the manager's ``create()`` method, in case the CollectedInput model has been swapped and
     requires additional fields to successfully save.
     """
-    CollectedInput = models.get_input_model()
 
     db_data = CollectedInput().serialize_data(data)  # FIXME: classmethod/staticmethod?
 
