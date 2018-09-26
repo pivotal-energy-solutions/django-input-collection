a79c8f274228c9bbbb452265a485a4044bd4b58f
Autumn <tonightslastsong@gmail.com>
2018-09-26 12:30:19 -0700

    Avoid imports when swapped model isn't activated

--- testproj/testproj/core/models/__init__.py
+++ testproj/testproj/core/models/__init__.py
@@ -1,2 +1,8 @@
+from django.conf import settings
+
 from .base import *
-from .mysql_json import *
+
+if settings.INPUT_COLLECTEDINPUT_MODEL == 'core.CollectedInput_MySQL_JSON':
+    from .mysql_json import *
+elif settings.INPUT_COLLECTEDINPUT_MODEL == 'core.CollectedInput_Postgres_JSON':
+    from .mysql_postgres import *
