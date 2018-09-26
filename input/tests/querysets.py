eea4debf8e3b82c9a016d5273197ffd500273a2b
Autumn <tonightslastsong@gmail.com>
2018-09-26 12:31:13 -0700

    Add context queryset tests

--- input/tests/querysets.py
+++ input/tests/querysets.py
@@ -1,10 +1,26 @@
 from django.test import TestCase
 
 from .. import models
+from ..models import managers
+from . import factories
 
 
 CollectedInput = models.get_input_model()
 
+
 class ContextualQuerySetTests(TestCase):
-    def test_static_context_applied_globally(self):
-        pass
+    def test_queryset_has_filter_for_context_method(self):
+        self.assertEqual(hasattr(CollectedInput.objects, 'filter_for_context'), True)
+
+    def test_static_context_applies_implicitly(self):
+        """ Tests that a queryset 'context' does at least a basic filter. """
+        factories.CollectedInputFactory.create_batch(size=2)
+
+        standard_queryset = CollectedInput.objects.all()
+        filtered_queryset = CollectedInput.objects.filter_for_context(**{
+            'id': 1,
+        })
+
+        self.assertEqual(standard_queryset.count(), 2)
+        self.assertEqual(filtered_queryset.count(), 1)
+        self.assertEqual(filtered_queryset.get().id, 1)
