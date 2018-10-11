17e37f6d5ad854a7877245c005412ed26568dd2c
Autumn <avalenta@pivotalenergysolutions.com>
2018-10-11 15:31:28 -0700

    Drop redundant modeladmins

--- testproj/testproj/core/admin.py
+++ testproj/testproj/core/admin.py
@@ -1,36 +1,7 @@
 from django.contrib import admin
 
-from django_input_collection.models import (Measure, CollectionGroup, CollectionRequest, CollectionInstrument,
-                          AbstractCollectedInput, get_input_model)
-
 from .models import Survey, PoliticalRally, RallyPoll
 
-
-
-@admin.register(Measure)
-class MeasureAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(CollectionGroup)
-class CollectionGroupAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(CollectionRequest)
-class CollectionRequestAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(CollectionInstrument)
-class CollectionInstrumentAdmin(admin.ModelAdmin):
-    list_display = ('text', 'group', 'measure', 'collection_request')
-    list_filter = ('group', 'measure')
-
-
-@admin.register(get_input_model())
-class CollectedInputAdmin(admin.ModelAdmin):
-    pass
 
 
 @admin.register(Survey)
