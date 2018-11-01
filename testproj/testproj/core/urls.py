03ebdb68ff60b22743b7964b84a34e82ef7dfeaa
Autumn <avalenta@pivotalenergysolutions.com>
2018-11-01 13:17:40 -0700

    So this is what happens when I'm sick

--- testproj/testproj/core/urls.py
+++ testproj/testproj/core/urls.py
@@ -4,9 +4,10 @@ from django_input_collection.compat import url, include
 from . import views
 
 urlpatterns = []
+
 if features.rest_framework:
     urlpatterns.extend([
         url(r'^polls/', include([
             url(r'^(?P<pk>\d+)/', views.PollView.as_view()),
         ])),
-    ]
+    ])
