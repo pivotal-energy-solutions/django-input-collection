ab7f63499f2c1ff2091654596d19fb88fd3e1d23
Autumn <avalenta@pivotalenergysolutions.com>
2018-11-01 12:53:35 -0700

    Remove payload modifier logic
    With custom serializers, the payload is back in developer-controlled territory

--- django_input_collection/api/restframework/api.py
+++ django_input_collection/api/restframework/api.py
@@ -94,34 +94,6 @@ class CollectedInputViewSet(CollectorEnabledMixin, viewsets.ModelViewSet):
     def get_queryset(self):
         return models.get_input_model().objects.all()
 
-    def _save_and_store_ref(self, serializer):
-        serializer.save()
-        self._instance = serializer.instance
-
-    perform_create = _save_and_store_ref
-    perform_update = _save_and_store_ref
-
-    def _rewrite_full_payload(self, response):
-        serializer = self.get_serializer(instance=self._instance, write_mode=False)
-        response.data = serializer.data
-        return response
-
-    def create(self, request, *args, **kwargs):
-        response = super(CollectedInputViewSet, self).create(request, *args, **kwargs)
-        return self._rewrite_full_payload(response)
-
-    def update(self, request, *args, **kwargs):
-        response = super(CollectedInputViewSet, self).update(request, *args, **kwargs)
-        return self._rewrite_full_payload(response)
-
-    def get_serializer(self, *args, **kwargs):
-        context_kwargs = {
-            'write_mode': kwargs.pop('write_mode', None),
-        }
-
-        serializer_class = self.get_serializer_class()
-        kwargs['context'] = self.get_serializer_context(**context_kwargs)
-        return serializer_class(*args, **kwargs)
 
     def get_serializer_context(self, write_mode=None):
         context = super(CollectedInputViewSet, self).get_serializer_context()
