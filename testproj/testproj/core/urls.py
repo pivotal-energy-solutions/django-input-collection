a8c40aa4cb94c670c2821bcd05419d3173f04c9e
Autumn <avalenta@pivotalenergysolutions.com>
2018-11-01 13:14:51 -0700

    Block drf view import if drf is not installed
    This wasn't a problem before by some miracle

--- testproj/testproj/core/urls.py
+++ testproj/testproj/core/urls.py
@@ -1,9 +1,12 @@
+from django_input_collection import features
 from django_input_collection.compat import url, include
 
 from . import views
 
-urlpatterns = [
-    url(r'^polls/', include([
-        url(r'^(?P<pk>\d+)/', views.PollView.as_view()),
-    ])),
-]
+urlpatterns = []
+if features.rest_framework:
+    urlpatterns.extend([
+        url(r'^polls/', include([
+            url(r'^(?P<pk>\d+)/', views.PollView.as_view()),
+        ])),
+    ]
