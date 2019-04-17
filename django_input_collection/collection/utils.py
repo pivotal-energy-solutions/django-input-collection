9379627fb34d1e5764cebeb1bdd08206b7269a28
Autumn <avalenta@pivotalenergysolutions.com>
2019-04-16 17:19:44 -0700

    Force a data-only lookup

--- django_input_collection/collection/utils.py
+++ django_input_collection/collection/utils.py
@@ -3,22 +3,26 @@ def expand_suggested_responses(instrument, lookups, *responses):
     Maps any {'_suggested_response': pk} values to the SuggestedResponse by that id, as long as it
     is present in the ``lookups`` dict.
     """
+    data_lookups = {bound_response.data: bound_response for bound_response in lookups.values()}
 
     values = []
     for response in responses:
-        data = response  # Assume raw passthrough by default
+        data = None
 
         # Transform data referring to a SuggestedResponse into that instance directly
-        if isinstance(data, dict) and '_suggested_response' in data:
-            bound_response_id = data['_suggested_response']
+        if isinstance(response, dict):
+            bound_response_id = response.get('_suggested_response')
             if bound_response_id in lookups:
                 data = lookups[bound_response_id]
-            else:
-                raise ValueError("[CollectionInstrument id=%r] Invalid bound response id=%r in choices: %r" % (
-                    instrument.id,
-                    bound_response_id,
-                    lookups,
-                ))
+        elif response in data_lookups:
+            data = data_lookups[response]
+
+        if data is None:
+            raise ValueError("[CollectionInstrument id=%r] Invalid bound response id=%r in choices: %r" % (
+                instrument.id,
+                bound_response_id,
+                lookups,
+            ))
 
         values.append(data)
 
