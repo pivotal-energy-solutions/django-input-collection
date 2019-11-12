0e8d5b2b8f864f2dcf9cf5ec5cecb133ae790d6d
Autumn Valenta <avalenta@pivotalenergysolutions.com>
2019-11-12 00:51:36 -0700

    Replace deprecated `base_name` kwarg

--- django_input_collection/api/restframework/urls.py
+++ django_input_collection/api/restframework/urls.py
@@ -9,16 +9,16 @@ from . import api
 router = routers.SimpleRouter()
 
 # Always available
-router.register(r'request', api.CollectionRequestViewSet, base_name='request')
-router.register(r'measure', api.MeasureViewSet, base_name='measure')
+router.register(r'request', api.CollectionRequestViewSet, basename='request')
+router.register(r'measure', api.MeasureViewSet, basename='measure')
 
 # Requires a collector in the request args
 # NOTE: The use of url parameter is tempting, but this is an explicit deference to the simplicity
 # in building data for a static url, not both the data and the url.
-router.register(r'segment', api.CollectionGroupViewSet, base_name='segment')
-router.register(r'group', api.CollectionGroupViewSet, base_name='group')
-router.register(r'instrument', api.CollectionInstrumentViewSet, base_name='instrument')
-router.register(r'input', api.CollectedInputViewSet, base_name='input')
+router.register(r'segment', api.CollectionGroupViewSet, basename='segment')
+router.register(r'group', api.CollectionGroupViewSet, basename='group')
+router.register(r'instrument', api.CollectionInstrumentViewSet, basename='instrument')
+router.register(r'input', api.CollectedInputViewSet, basename='input')
 
 app_name = 'collection-api'
 urlpatterns = router.urls
