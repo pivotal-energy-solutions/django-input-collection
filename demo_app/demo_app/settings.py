d1ba5d429c337740f750d9d50450688681631a9d
Steven K <steven@pointcircle.com>
2022-04-18 19:56:52 -0700

    Bug Fix.

--- demo_app/demo_app/settings.py
+++ demo_app/demo_app/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9n_o$m)q@)p)b_(5e2jnd#)tup2b7a_aa%q44h2msp)e4w5krb"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
