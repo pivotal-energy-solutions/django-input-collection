eaaf09461969a11dfe6e4f05a7ddb727c1d2a3b1
Steven K <steven@pointcircle.com>
2022-04-18 19:23:41 -0700

    Updated final

--- demo_app/manage.py
+++ demo_app/manage.py
@@ -7,9 +7,6 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    if os.path.abspath(os.path.join(os.path.dirname(__file__), "..")) not in sys.path:
-        sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo_app.settings")
     try:
         from django.core.management import execute_from_command_line
