68f6c19fcf3a4d4cf299c5082e9f38fd2c82ca86
Steven K <steven@pointcircle.com>
2024-06-29 16:20:24 +0000

    Version Bump [skip ci]

--- django_input_collection/__init__.py
+++ django_input_collection/__init__.py
@@ -2,8 +2,8 @@
 
 __name__ = "django_input_collection"
 __author__ = "Pivotal Energy Solutions"
-__version_info__ = (6, 0, 7)
-__version__ = "6.0.7"
+__version_info__ = (6, 0, 8)
+__version__ = "6.0.8"
 __date__ = "2014/07/22 4:47:00 PM"
 __credits__ = ["Steven Klass", "Autumn Valenta"]
 __license__ = "See the file LICENSE.txt for licensing information."
